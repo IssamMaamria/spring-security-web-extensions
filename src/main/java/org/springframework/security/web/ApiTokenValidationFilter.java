2054d6e77d8b6b329b795cdf67854bde2feb0246
Maamria <maamria@gmail.com>
2015-10-15 17:51:58 +0100

    naming issue

--- src/main/java/org/springframework/security/web/ApiTokenValidationFilter.java
+++ src/main/java/org/springframework/security/web/ApiTokenValidationFilter.java
@@ -28,9 +28,9 @@ import java.io.IOException;
  *         <p>
  *         07/10/15.
  */
-public class ApiTokenValidationFilter2 extends GenericFilterBean {
+public class ApiTokenValidationFilter extends GenericFilterBean {
 
-    private static final Logger log = LoggerFactory.getLogger(ApiTokenValidationFilter2.class);
+    private static final Logger log = LoggerFactory.getLogger(ApiTokenValidationFilter.class);
 
     private ApiSecurityConfig apiSecurityConfig = new DefaultApiSecurityConfig();
 
